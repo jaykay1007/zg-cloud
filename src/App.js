cd236db7b7e5f901b536d3c7969e53931a1b9018
jk <jayakkumar.k@siamcomputing.com>
2023-08-02 18:50:16 +0530

    comments added

--- src/App.js
+++ src/App.js
@@ -12,39 +12,52 @@ export function getUrlParams(
 }
 
 export default function App() {
-
-      let myMeeting = async (element) => {
-     // generate Kit Token
-      const appID = 167959465;
-      const roomID = "testCallID"; //static roomId
-      const serverSecret = "1c7c018987d37f50cf8adbbfe9909415";
-      const userName = "jk";
-      const userId = "12345";
-      const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, userId, userName,10000000);
-     // Create instance object from Kit Token.
-      const zp = ZegoUIKitPrebuilt.create(kitToken);
-      // start the call
-      zp.joinRoom({
-        container: element,
-        sharedLinks: [
-          {
-            name: 'Personal link', // can be any name
-            url:
-             window.location.protocol + '//' + 
-             window.location.host + window.location.pathname +
-              '?roomID=' +
-              roomID,
-          },
-        ],
-        scenario: {
-          mode: ZegoUIKitPrebuilt.OneONoneCall, 
+/**
+ * This function initializes and joins a meeting using the ZegoUIKitPrebuilt library.
+ * It generates a kit token and creates a ZegoUIKitPrebuilt instance to join the room.
+ * It also sets up the shared links and the scenario mode for the call.
+ * The meeting container is set using a ref.
+ * 
+ * @param {Element} element - The DOM element that serves as the meeting container.
+ */
+  const myMeeting = async (element) => {
+    // Set the required parameters for the meeting
+    const appID = 167959465;
+    const roomID = "testCallID";
+    const serverSecret = "1c7c018987d37f50cf8adbbfe9909415";
+    const userName = "jk";
+    const userId = "12345";
+  
+    // Generate the kit token for the meeting
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      userId,
+      userName,
+      10000000
+    );
+  
+    // Create a ZegoUIKitPrebuilt instance using the kit token
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+  
+    // Join the room with the specified settings
+    zp.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: 'Personal link',
+          url: `${window.location.protocol}//${window.location.host}${window.location.pathname}?roomID=${roomID}`,
         },
-      });
-
-
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.OneONoneCall,
+      },
+    });
   };
 
   return (
+    // Set the meeting container using a ref
     <div
       ref={myMeeting}
       style={{ width: '100vw', height: '100vh' }}

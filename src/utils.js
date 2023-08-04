1ad7dec849c2c08d20c30bc5057a1768b83c4884
jk <jayakkumar.k@siamcomputing.com>
2023-08-04 11:15:02 +0530

    plain js zego call

--- src/utils.js
+++
@@ -1,3 +0,0 @@
-export function generateToken(userId,roomID,userName) {
-    return fetch('https://choui-prebuilt.herokuapp.com/access_token?userId=${userId}&roomID=${roomID}&userName=${userName}').then((res)=>res.json()) 
-}
